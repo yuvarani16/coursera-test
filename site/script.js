fbbc3a8ca50056dc3f59386bdaddc5d0780accb3
yuvarani16 <raniyuva833@gmail.com>
2020-06-11 12:14:39 +0530

    small addition

--- site/script.js
+++ site/script.js
@@ -1,11 +1,11 @@
 (function(window){
 	var names=["yaaakov","John","Jen","Jack","Paul","Fake","Lorry","Laisi","Jaya"];
-	for(var i=0; i < names.length ; i++){
+	for (var i in names) {
 		 var firstLetter = names[i].charAt(0).toLowerCase();
 		if(firstletter=='j'){
-			window.byespeaker.speak(name[i]);
+			window.byespeaker.speak(names[i]);
 		} else{
-			window.hellospeaker.speak(name[i]);
+			window.hellospeaker.speak(names[i]);
 		}
 	}
 })(window);
