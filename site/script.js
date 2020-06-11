ee9af1d263bf51b2f8fb0d77df89bbaa90966a9a
yuvarani16 <raniyuva833@gmail.com>
2020-06-11 12:18:14 +0530

    small addition

--- site/script.js
+++ site/script.js
@@ -2,7 +2,7 @@
 	var names=["yaaakov","John","Jen","Jack","Paul","Fake","Lorry","Laisi","Jaya"];
 	for (var i in names) {
 		 var firstLetter = names[i].charAt(0).toLowerCase();
-		if(firstletter=='j'){
+		if(firstLetter=='j'){
 			window.byespeaker.speak(names[i]);
 		} else{
 			window.hellospeaker.speak(names[i]);
