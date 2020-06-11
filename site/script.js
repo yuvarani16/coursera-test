5028e1e2945f09fddbea815ec1bca1544acb62f2
yuvarani16 <raniyuva833@gmail.com>
2020-06-11 10:46:18 +0530

    small addition

--- site/script.js
+++ site/script.js
@@ -1,12 +1,2 @@
-var name=["yaakov","joe","rio"]
-switch(name)
-{	case joe:
-		console.log("Goodbye joe");
-		break;
-	case yakkov:
-	case rio:
-		console.log("Hello somename");
-		break;
-	default:
-	console.log("nothing");
-}
+var name=["yaakov","joe","rio"];
+console.log(name);
