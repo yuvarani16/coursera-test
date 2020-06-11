abd919264a1ffc3207fd57b9435349f732b08866
yuvarani16 <raniyuva833@gmail.com>
2020-06-11 11:01:21 +0530

    small addition

--- site/script.js
+++ site/script.js
@@ -1,5 +1,5 @@
 var name=["yaakov","raj","rio"];
 console.log(name);
-for(var i=0,i<name.length,i++){
-	console.log("Goodbye"+name[i]);
+for(var i=0;i<name.length;i++){
+	console.log("Goodbye "+name[i]);
 }
